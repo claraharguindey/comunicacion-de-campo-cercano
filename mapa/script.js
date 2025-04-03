48392974f93919a452e02d19204754a8d0608d91
claraharguindey <clarisaharguindey@gmail.com>
2025-04-03 19:12:11 +0200

    Add images

--- mapa/script.js
+++ mapa/script.js
@@ -1,20 +1,73 @@
-function openModal(areaName, event) {
-  var modal = document.getElementById("myModal");
-  var modalTitle = document.getElementById("modalTitle");
-  modalTitle.textContent = "Área: " + areaName;
+const openModal = (areaName, event) => {
+  const lugares = {
+    iragi: {
+      title: "Iragi",
+      image: "./images/iragi.jpg",
+    },
+    eugi: {
+      title: "Eugi",
+      image: "./images/eugi.png",
+    },
+    sarasibar: {
+      title: "Sarasíbar",
+      image: "./images/sarasibar.png",
+    },
+    irotz: {
+      title: "Irotz",
+      image: "./images/irotz.jpg",
+    },
+    ilurdotz: {
+      title: "Ilurdotz",
+      image: "./images/ilurdotz.jpg",
+    },
+    errea: {
+      title: "Errea",
+      image: "./images/errea.jpg",
+    },
+    urtasun: {
+      title: "Urtasun",
+      image: "./images/urtasun.png",
+    },
+    larrasoana: {
+      title: "Larrasoaña",
+      image: "./images/larrasoana.png",
+    },
+    angorreta: {
+      title: "Angorreta",
+      image: "./images/angorreta.jpg",
+    },
+    urdaniz: {
+      title: "Urdániz",
+      image: "./images/urdaniz.jpg",
+    },
+    leranotz: {
+      title: "Leranotz",
+      image: "./images/leranotz.jpg",
+    },
+    zubiri: {
+      title: "Zuribi",
+      image: "./images/zubiri.jpg",
+    },
+  };
+
+  const modal = document.getElementById("modal");
+  const modalTitle = document.getElementById("modalTitle");
+  const modalImage = document.getElementById("modalImage");
+  modalTitle.textContent = lugares[areaName].title;
+  modalImage.src = lugares[areaName].image;
 
   if (window.innerWidth >= 768) {
-    var cursorX = event.clientX;
-    var cursorY = event.clientY;
+    let cursorX = event.clientX;
+    let cursorY = event.clientY;
 
-    var modalWidth = modal.offsetWidth;
-    var modalHeight = modal.offsetHeight;
+    let modalWidth = modal.offsetWidth;
+    let modalHeight = modal.offsetHeight;
 
-    var windowWidth = window.innerWidth;
-    var windowHeight = window.innerHeight;
+    let windowWidth = window.innerWidth;
+    let windowHeight = window.innerHeight;
 
-    var modalLeft = cursorX + 10;
-    var modalTop = cursorY + 10;
+    let modalLeft = cursorX + 10;
+    let modalTop = cursorY + 10;
 
     if (modalLeft + modalWidth > windowWidth) {
       modalLeft = windowWidth - modalWidth - 10;
@@ -29,16 +82,14 @@ function openModal(areaName, event) {
   }
 
   modal.style.display = "block";
-}
+};
 
-// Función para cerrar el modal
-function closeModal() {
-  document.getElementById("myModal").style.display = "none";
-}
+const closeModal = () => {
+  document.getElementById("modal").style.display = "none";
+};
 
-// Cerrar el modal si se hace clic fuera de la ventana del modal
-window.onclick = function (event) {
-  if (event.target == document.getElementById("myModal")) {
+window.onclick = (event) => {
+  if (event.target == document.getElementById("modal")) {
     closeModal();
   }
 };
